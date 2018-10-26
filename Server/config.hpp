839c9dc7679b84369506e68dcfd18627ee2443b6
sppmacd <sppmacd@gmail.com>
2018-10-26 17:00:40 +0200

    Fixed project setup

--- Server/config.hpp
+++ Server/config.hpp
@@ -1,13 +1,6 @@
 #ifndef CONFIG_HPP_INCL
 #define CONFIG_HPP_INCL
 
-#ifdef CHPP_INCLUDE_ALL
-#define CHPP_INCLUDE_STD
-#define CHPP_INCLUDE_SFML
-#endif // CHPP_INCLUDE_ALL
-
-#ifdef CHPP_INCLUDE_STD
-
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
@@ -15,14 +8,8 @@
 #include <vector>
 using namespace std;
 
-#endif // std
-#ifdef CHPP_INCLUDE_SFML
-
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
 using namespace sf;
 
-#endif
 #endif // CONFIG_HPP_INCL
 
