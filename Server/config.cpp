57bd05a4e287e71dd91d0c1b851bc5161f176b32
sppmacd <sppmacd@gmail.com>
2018-10-27 13:34:54 +0200

    Added permissions

--- Server/config.cpp
+++ Server/config.cpp
@@ -7,34 +7,31 @@ namespace pms
     {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorCode);
         cout << str;
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x00);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0x0F);
     }
 
     void log(LogLevel level, string str)
     {
         time_t t = time(NULL);
         string ct = ctime(&t);
-        if(level != Debug)
-            cout << "[" << ct.substr(0, ct.size() - 2) << "] ";
-        else
-            displayColored(0x08, "[" + ct.substr(0, ct.size() - 2) + "] ");
 
         switch(level)
         {
         case Info:
-            cout << "INFO: ";
+            displayColored(0x07, "INFO: [" + ct.substr(0, ct.size() - 2) + "] ");
             displayColored(0x0F, str);
             break;
         case Warning:
-            cout << "WARN: ";
+            displayColored(0x0E, "WARN: [" + ct.substr(0, ct.size() - 2) + "] ");
             displayColored(0x06, str);
             break;
         case Error:
-            cout << "ERROR: ";
+            displayColored(0x04, "ERROR: [" + ct.substr(0, ct.size() - 2) + "] ");
             displayColored(0x0C, str);
             break;
         case Debug:
-            displayColored(0x08, "DEBUG: " + str);
+            displayColored(0x08, "DEBUG: [" + ct.substr(0, ct.size() - 2) + "] ");
+            displayColored(0x08, str);
             break;
         }
         cout << endl;
